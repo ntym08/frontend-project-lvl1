34df399356d88e579d22aa65411d5cdbb821c0d4
Nataly Lapina <lapinann08@gmail.com>
2021-07-22 18:07:07 +0300

    add logic of game brain-even

---
+++ src/games/even.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const minNum = 1;
+const maxNum = 100;
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const isEven = (number) => number % 2 === 0;
+
+const checkEven = () => {
+  const ruleMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
+  console.log(ruleMessage);
+  for (let i = 1; i < 4; i += 1) {
+    const randomNum = getRandomNumber(minNum, maxNum);
+    const questionMessage = `Question: ${randomNum}`;
+    console.log(questionMessage);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const expectedAnswer = isEven(randomNum) ? 'yes' : 'no';
+    if (userAnswer === expectedAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${expectedAnswer}".`);
+      console.log("Let's try again, Bill!");
+      break;
+    }
+    if (i === 3) console.log('Congratulations, Bill!');
+  }
+};
+
+export default checkEven;
